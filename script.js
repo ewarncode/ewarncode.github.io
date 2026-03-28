bff751f0c9071609eb5073c6d9cc7560a066b8b5
ewarncode <ewarncode@gmail.com>
2026-03-28 12:30:08 +0530

    web

---
+++ script.js
@@ -0,0 +1,16 @@
+const searchBox = document.getElementById("searchBox");
+const cards = document.querySelectorAll(".card");
+
+searchBox.addEventListener("keyup", function () {
+    let value = searchBox.value.toLowerCase();
+
+    cards.forEach(function (card) {
+        let text = card.innerText.toLowerCase();
+
+        if (text.includes(value)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
